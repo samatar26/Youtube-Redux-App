3a53492a58e92dd1ec536c2170b34168db43bebc
Samatar Axmed <Samatar_267@hotmail.com>
2017-05-27 18:48:01 +0100

    Created Homepage Component Relates to #8

---
+++ src/components/base/MainContent.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const MainContent = styled.main`
+  max-width: 40em;
+  margin: 0 auto;
+  padding: 1em;
+  & > * + * {
+    margin-top: 3em;
+  }
+  @media (min-width: 40em) {
+    padding: 2em;
+  }
+`;
